96b4c3f52b2f8c64794d18eb12dfbeed3cffcebb
Kozlov Sergey <sergeykozlov4545@gmail.com>
2018-07-30 16:59:52 +0500

    Генерация точек когда список точек пуст

--- app/src/main/java/com/example/sergey/geofencingwithrealmapplication/Presenter/edit/EditPointPresenterImpl.java
+++ app/src/main/java/com/example/sergey/geofencingwithrealmapplication/Presenter/edit/EditPointPresenterImpl.java
@@ -2,6 +2,7 @@ package com.example.sergey.geofencingwithrealmapplication.Presenter.edit;
 
 import android.support.annotation.NonNull;
 
+import com.example.sergey.geofencingwithrealmapplication.Model.RealmLatLng;
 import com.example.sergey.geofencingwithrealmapplication.Model.Region;
 import com.example.sergey.geofencingwithrealmapplication.Model.RegionsDatabase;
 import com.example.sergey.geofencingwithrealmapplication.Presenter.base.BasePresenter;
@@ -28,6 +29,18 @@ public class EditPointPresenterImpl
 
         if (view == null) {
             return;
+        }
+
+        if (regionsDatabase.getRegions().isEmpty()) {
+            regionsDatabase.addRegion("Технология", new RealmLatLng(55.420859, 65.273978), 400);
+            regionsDatabase.addRegion("Центральный вокзал", new RealmLatLng(55.441445, 65.319457), 400);
+            regionsDatabase.addRegion("Некрасово", new RealmLatLng(55.452881, 65.340784), 400);
+            regionsDatabase.addRegion("Рио", new RealmLatLng(55.428296, 65.299177), 400);
+            regionsDatabase.addRegion("Горсад", new RealmLatLng(55.439788, 65.344471), 400);
+            regionsDatabase.addRegion("Кольцо у гаражей", new RealmLatLng(55.451240, 65.372292), 400);
+            regionsDatabase.addRegion("ЦПКиО", new RealmLatLng(55.430798, 65.326772), 400);
+            regionsDatabase.addRegion("Кировский пляж", new RealmLatLng(55.430760, 65.349454), 400);
+            regionsDatabase.addRegion("Молодежный", new RealmLatLng(55.439380, 65.364141), 400);
         }
 
         OrderedRealmCollection<Region> regions = regionsDatabase.getRegions();
